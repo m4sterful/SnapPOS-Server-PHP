dc6a656130c4b63889a4727b5b517de8f176c301
m4sterful <phil@ccomp.ca>
2026-03-20 21:10:21 -0400

    add system to the common route creation loop

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('setup.complete')->group(function (): void {
     Route::get('/', ApplicationStatusController::class)->name('home');
 
-    Route::name('modules.system.')
-        ->prefix('system')
-        ->group(base_path('routes/api/system.php'));
-
-    foreach (['admin', 'marketing', 'purchasing', 'delivery', 'reporting', 'inventory', 'warehouse', 'operations'] as $domain) {
+    foreach (['system','admin', 'marketing', 'purchasing', 'delivery', 'reporting', 'inventory', 'warehouse', 'operations'] as $domain) {
         Route::name("modules.{$domain}.")
             ->prefix($domain)
             ->group(base_path("routes/api/{$domain}.php"));
